08252d7e2903c81daf8c7ae0c3cb7cd247a97258
tomermsh <tomerm223@gmail.com>
2017-03-05 22:15:51 +0200

    Download a track by POST request

--- backer.py
+++ backer.py
@@ -1,15 +1,33 @@
-#!/usr/bin/python
-
-from fetch import fetch
-import sys
+from flask import Flask, jsonify, request
+from fetch import fetch_tracks
 import pafy
 import webbrowser
+import random
 
-search_results = fetch(sys.argv[1])
-url = "http://www.youtube.com/watch?v=" + search_results[0]
-video = pafy.new(url)
-bestaudio = video.getbestaudio()
-filename = bestaudio.download(quiet=True)
+app = Flask(__name__)
 
-print(filename)
-webbrowser.open(filename)
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/download',methods=['POST'])
+def download_track():
+    try:
+        genre = request.form['genre']
+        key = request.form['key']
+        bpm = request.form['bpm']
+
+        search_results = fetch_tracks('backing track ' + genre + ' ' + key + ' ' + bpm)
+        url = "http://www.youtube.com/watch?v=" + random.choice(search_results[:10])
+        video = pafy.new(url)
+        bestaudio = video.getbestaudio()
+        filename = bestaudio.download(quiet=True)
+
+        return jsonify(status='ok',message='Downloaded "' + filename + '"')
+
+    except Exception,e:
+        return jsonify(status='error',message=str(e))
--- fetch.py
+++ fetch.py
@@ -2,7 +2,7 @@ import urllib
 import urllib2
 import re
 
-def fetch(query):
+def fetch_tracks(query):
     query_string = urllib.urlencode({"search_query" : query})
     html_content = urllib2.urlopen("http://www.youtube.com/results?" + query_string)
     search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read())
